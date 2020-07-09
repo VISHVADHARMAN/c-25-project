2f700ca84acab1c86a525f25fe0bae877f1ee990
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2020-07-09 18:03:28 +0530

    all files are uploaded

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	
+	
+	paperObject = new Paper(200,450,70);
+	
+	ground1 = new Ground(width/2,670,width,20);
+
+	dustbin1 = new Dustbin(1200,650);
+	Engine.run(engine);
+    
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(180);
+  
+  paperObject.display();
+  ground1.display();
+  dustbin1.display();
+  
+  
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-100})
+	}
+}
+
+
